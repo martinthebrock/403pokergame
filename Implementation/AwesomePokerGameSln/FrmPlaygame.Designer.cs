5be92a57f021606b3bcf2c2cc5811f630d737784
Chris Sanders <javac056@gmail.com>
2018-11-07 18:42:40 -0600

    added labels for player's and dealer's cards

--- Implementation/AwesomePokerGameSln/FrmPlaygame.Designer.cs
+++ Implementation/AwesomePokerGameSln/FrmPlaygame.Designer.cs
@@ -42,6 +42,8 @@ namespace AwesomePokerGameSln {
             this.pictureBox5 = new System.Windows.Forms.PictureBox();
             this.btnViewRuleBook = new System.Windows.Forms.Button();
             this.picRulebook = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.picCard1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCard3)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.picCard4)).BeginInit();
@@ -206,11 +208,34 @@ namespace AwesomePokerGameSln {
             this.picRulebook.TabStop = false;
             this.picRulebook.Visible = false;
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(11, 58);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Dealer";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(26, 230);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "You";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // FrmPlaygame
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.picRulebook);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.pictureBox2);
@@ -264,5 +289,7 @@ namespace AwesomePokerGameSln {
     private System.Windows.Forms.PictureBox pictureBox5;
     private System.Windows.Forms.Button btnViewRuleBook;    //rulebook
     private System.Windows.Forms.PictureBox picRulebook;    //rulebook
+        private Label label1;
+        private Label label2;
     }
 }
--- Implementation/AwesomePokerGameSln/FrmPlaygame.cs
+++ Implementation/AwesomePokerGameSln/FrmPlaygame.cs
@@ -105,5 +105,10 @@ namespace AwesomePokerGameSln {
             btnViewRuleBook.Text = "View Rule Book";
         }
     }
-  }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
 }
